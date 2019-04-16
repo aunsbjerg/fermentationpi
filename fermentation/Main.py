98fd2badd8841577b0f10df5f13cc2472aa916aa
Mikkel Jakobsen <mikkelaunsbjerg@gmail.com>
2019-04-16 17:34:30 +0200

    main: testing pt100 and ssr

--- fermentation/Main.py
+++ fermentation/Main.py
@@ -1,9 +1,8 @@
 """
 Simple little application for getting/setting chiller and temperature over
-http. 
+http.
 
 Todo:
-    - implement MAX31865 driver
     - implement temperature control loop
     - integrate with tilt device (see: tiltpi implementation)
     - async implementation using twisted/tornado
@@ -13,14 +12,16 @@ Todo:
 import time
 import click
 import ptvsd
-from gpiozero import DigitalOutputDevice
+import Rpi.GPIO as GPIO
 from Drivers.SolidStateRelay import SolidStateRelay
 from Drivers.MAX31865 import MAX31865
+
 
 def debug_server(debug_port):
     print('Waiting for remote debugging ...')
     ptvsd.enable_attach(address=('192.168.0.19', debug_port), redirect_output=True)
     ptvsd.wait_for_attach()
+
 
 @click.command()
 @click.option('--debug', is_flag=True, help='enable remote debugging')
@@ -30,14 +31,18 @@ def main(debug, debug_port):
     if debug:
         debug_server(debug_port)
 
-    print("hello")
-    pt100 = MAX31865(wires=2, port=0, device=0)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    pt100 = MAX31865(cs_pin=8, miso_pin=9, mosi_pin=10, clk_pin=11)
+    ssr = SolidStateRelay(pin=18, active_high=True, initial_state=False)
 
     while True:
         time.sleep(1.0)
-        print("rtd         {}".format(pt100._read_rtd()))
-        print("resistance  {}".format(pt100._read_resistance()))
-        print("temperature {}".format(pt100.temperature))
+        print("temperature {}".format(pt100.temperature()))
+        ssr.toggle()
+
+    GPIO.cleanup()
 
 
 if __name__ == '__main__':
