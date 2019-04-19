974fa82ac46dde63a2d0d5a4a407e6f7835a6c2d
Mikkel Jakobsen <mikkelaunsbjerg@gmail.com>
2019-04-19 15:07:08 +0200

    main: simplified cleanup, added log to file

--- fermentation/Main.py
+++ fermentation/Main.py
@@ -14,31 +14,32 @@ from TemperatureControl import TemperatureControl
 from Drivers.Factories import relay_factory, temperature_factory
 
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.DEBUG)
+def configure_logger(logpath, loglevel=logging.DEBUG):
+    """
+    Configure the logging system.
+    If a logpath is provided, entries will also be written to that logfile.
+    """
+    handlers = [logging.StreamHandler()]
+
+    if logpath:
+        handlers.append(logging.FileHandler(logpath))
+
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+        datefmt='%d-%m-%y %H:%M:%S', level=loglevel, handlers=handlers)
+
+
 logger = logging.getLogger(__name__)
-
-
-def cleanup():
-    """
-    Simple routine to call after exceptions or on program halt to make sure that relays are in off position
-    and that gpio has been deinitialized.
-    """
-    try:
-        compressor_relay.off()
-
-    except:
-        pass
-
-    GPIO.cleanup()
 
 
 @click.command()
 @click.option('--configpath', type=click.Path(), help='configuration file location')
+@click.option('--logpath', type=click.Path(), help='log output file location')
 @click.option('--setpoint', default=18.0, show_default=True, help='temperature setpoint in °C')
-def main(configpath, setpoint):
+def main(configpath, logpath, setpoint):
     """
     _tbd_
     """
+    configure_logger(logpath)
     logger.info('starting application')
 
     try:
@@ -65,7 +66,8 @@ def main(configpath, setpoint):
     except Exception:
         logger.error('Exception occured', exc_info=True)
 
-    cleanup()
+    compressor_relay.off()
+    GPIO.cleanup()
 
 
 if __name__ == '__main__':
