a31c6cf2a9a516821a0f95563d66ab38f04fd21f
Mikkel Jakobsen <mikkelaunsbjerg@gmail.com>
2019-04-19 14:51:36 +0200

    main: reworked to use factories, config and temp control modules

--- fermentation/Main.py
+++ fermentation/Main.py
@@ -1,48 +1,71 @@
 """
-Simple little application for getting/setting chiller and temperature over
-http.
-
 Todo:
-    - implement temperature control loop
     - integrate with tilt device (see: tiltpi implementation)
     - async implementation using twisted/tornado
     - add web based ui
     - integrate with brewersfriend api
 """
+import logging
 import time
 import click
-import ptvsd
-import Rpi.GPIO as GPIO
-from Drivers.SolidStateRelay import SolidStateRelay
-from Drivers.MAX31865 import MAX31865
+import RPi.GPIO as GPIO
+from Configuration import import_configuration, default_configuration
+from TemperatureControl import TemperatureControl
+from Drivers.Factories import relay_factory, temperature_factory
 
 
-def debug_server(debug_port):
-    print('Waiting for remote debugging ...')
-    ptvsd.enable_attach(address=('192.168.0.19', debug_port), redirect_output=True)
-    ptvsd.wait_for_attach()
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def cleanup():
+    """
+    Simple routine to call after exceptions or on program halt to make sure that relays are in off position
+    and that gpio has been deinitialized.
+    """
+    try:
+        compressor_relay.off()
+
+    except:
+        pass
+
+    GPIO.cleanup()
 
 
 @click.command()
-@click.option('--debug', is_flag=True, help='enable remote debugging')
-@click.option('--debug-port', default=3000, show_default=True, help='remote debugging port')
-def main(debug, debug_port):
+@click.option('--configpath', type=click.Path(), help='configuration file location')
+@click.option('--setpoint', default=18.0, show_default=True, help='temperature setpoint in °C')
+def main(configpath, setpoint):
+    """
+    _tbd_
+    """
+    logger.info('starting application')
 
-    if debug:
-        debug_server(debug_port)
+    try:
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
 
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
+        config = import_configuration(configpath) if configpath else default_configuration()
 
-    pt100 = MAX31865(cs_pin=8, miso_pin=9, mosi_pin=10, clk_pin=11)
-    ssr = SolidStateRelay(pin=18, active_high=True, initial_state=False)
+        beer_temp = temperature_factory(config['beer_temperature'])
+        fridge_temp = temperature_factory(config['fridge_temperature'])
+        compressor_relay = relay_factory(config['compressor_relay'])
 
-    while True:
-        time.sleep(1.0)
-        print("temperature {}".format(pt100.temperature()))
-        ssr.toggle()
+        temp_control = TemperatureControl(fridge_temp, beer_temp, compressor_relay)
+        temp_control.set_temperature_setpoint(setpoint)
+        temp_control.start()
 
-    GPIO.cleanup()
+        while True:
+            time.sleep(1.0)
+            temp_control.control_loop()
+
+    except KeyboardInterrupt:
+        logger.warning('CTRL+C detected, stopping')
+
+    except Exception:
+        logger.error('Exception occured', exc_info=True)
+
+    cleanup()
 
 
 if __name__ == '__main__':
